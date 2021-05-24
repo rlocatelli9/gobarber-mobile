64c976b4b58a52fdf527780ffb028386c23fcf6f
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-24 18:07:57 -0300

    feat: criando paginas e rotas

---
+++ src/pages/AppointmentCreated/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const AppointmentCreated: React.FC = () => <View />;
+
+export default AppointmentCreated;
---
+++ src/pages/CreateAppointment/index.tsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import { View } from 'react-native';
+
+// import { Container } from './styles';
+
+const CreateAppointment: React.FC = () => <View />;
+
+export default CreateAppointment;
