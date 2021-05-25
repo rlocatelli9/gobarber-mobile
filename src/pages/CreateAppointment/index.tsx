ce1d9cf7cb24c6fa90d37bb373b1432479011cfa
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-25 19:21:03 -0300

    feat: Estrutura de criaçao

--- src/pages/CreateAppointment/index.tsx
+++ src/pages/CreateAppointment/index.tsx
@@ -1,8 +1,41 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import React, { useCallback } from 'react';
+import { StatusBar } from 'react-native';
+import Icon from 'react-native-vector-icons/Feather';
+import { useAuth } from '../../context/AuthContext';
 
-// import { Container } from './styles';
+import {
+  Container, Header, BackButton, HeaderTitle, UserAvatar
+} from './styles';
 
-const CreateAppointment: React.FC = () => <View />;
+interface RouteParams{
+  providerId: string;
+}
+
+StatusBar.setBackgroundColor('#28262e');
+
+const CreateAppointment: React.FC = () => {
+  const { user } = useAuth();
+  const route = useRoute();
+  const { goBack } = useNavigation();
+  const { providerId } = route.params as RouteParams;
+
+  const handleBackButton = useCallback(() => {
+    goBack();
+  }, [goBack]);
+
+  return (
+    <Container>
+      <Header>
+        <BackButton onPress={handleBackButton}>
+          <Icon name="chevron-left" size={24} color="#999591" />
+        </BackButton>
+        <HeaderTitle>Cabeleireiros</HeaderTitle>
+
+        <UserAvatar source={{ uri: user.avatarUrl }} />
+      </Header>
+    </Container>
+  );
+};
 
 export default CreateAppointment;
