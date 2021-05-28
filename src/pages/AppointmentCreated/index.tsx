75c5cf35a953c7f66a15ff49dca870a10d51bcb6
Robson Locatelli <robson.locatelli.m@gmail.com>
2021-05-28 14:10:08 -0300

    feat: criando tela e formatando data

--- src/pages/AppointmentCreated/index.tsx
+++ src/pages/AppointmentCreated/index.tsx
@@ -1,8 +1,52 @@
-import React from 'react';
-import { View } from 'react-native';
+import { useNavigation, useRoute } from '@react-navigation/native';
+import { format } from 'date-fns';
+import ptBR from 'date-fns/locale/pt-BR';
 
-// import { Container } from './styles';
+import React, { useCallback, useMemo } from 'react';
+import Icon from 'react-native-vector-icons/Feather';
 
-const AppointmentCreated: React.FC = () => <View />;
+import {
+  Container,
+  CustomTitle,
+  Description,
+  AgreeButton,
+  AgreeButtonText
+} from './styles';
+
+interface RouteParams {
+  date: number;
+}
+
+const AppointmentCreated: React.FC = () => {
+  const { reset } = useNavigation();
+  const { params } = useRoute();
+  const routeParams = params as RouteParams;
+
+  const formatedDate = useMemo(() => format(
+    routeParams.date, "EEEE', dia' dd 'de' MMMM 'de' yyyy 'às' HH'h'",
+    { locale: ptBR }
+  ), [routeParams]);
+
+  const handleAgreePressed = useCallback(() => {
+    reset({
+      routes: [
+        { name: 'Dashboard' },
+      ],
+      index: 0,
+    });
+  }, [reset]);
+
+  return (
+    <Container>
+      <Icon name="check" size={80} color="#04d361" />
+      <CustomTitle>Agendamento concluído</CustomTitle>
+      <Description>{formatedDate}</Description>
+
+      <AgreeButton onPress={handleAgreePressed}>
+        <AgreeButtonText>Ok!</AgreeButtonText>
+      </AgreeButton>
+    </Container>
+  );
+};
 
 export default AppointmentCreated;
